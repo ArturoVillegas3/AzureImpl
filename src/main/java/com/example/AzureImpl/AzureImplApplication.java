ebf855e44c18b7c27410b62239eebb848761397c
ArturoVillegas3 <percentnevada3@gmail.com>
2022-05-17 11:50:52 -0500

    first commit

---
+++ src/main/java/com/example/AzureImpl/AzureImplApplication.java
@@ -0,0 +1,13 @@
+package com.example.AzureImpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AzureImpl/AzureImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureImpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
